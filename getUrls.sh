2f66eb0a46c226686a1de3ab77f12c983e9631a4
mdaoustUdeS <4569756-mdaoustUdeS@users.noreply.gitlab.com>
2022-01-22 07:12:13 -0500

    Fix file overriding

--- getUrls.sh
+++ getUrls.sh
@@ -1,4 +1,4 @@
 outputDirectory=update
 
 url="https://covid19.quebec.ca/vaxiupdates/vaxi.json"
-wget --directory-prefix=$outputDirectory --backups 0 -- $url
+wget --directory-prefix=$outputDirectory -O vaxi.json -- $url
