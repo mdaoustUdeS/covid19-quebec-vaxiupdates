3c902ffbbace4306a79cefb7c10284196aff969b
mdaoustUdeS <54960692+mdaoustUdeS@users.noreply.github.com>
2022-01-22 15:03:25 -0500

    Format jwks

--- getUrls.sh
+++ getUrls.sh
@@ -3,3 +3,5 @@ wget -O update/vaxi.json -- $url
 
 url="https://covid19.quebec.ca/PreuveVaccinaleApi/issuer/.well-known/jwks.json"
 wget -O update/jwks.json -- $url
+
+jq . jwks.json > jwks-formated.json
