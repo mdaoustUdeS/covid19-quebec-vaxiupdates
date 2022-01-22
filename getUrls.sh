38164532f1c593efb983850fa7e3314528266b47
mdaoustUdeS <4569756-mdaoustUdeS@users.noreply.gitlab.com>
2022-01-22 07:19:16 -0500

    Fix path

--- getUrls.sh
+++ getUrls.sh
@@ -1,4 +1,4 @@
 outputDirectory=update
 
 url="https://covid19.quebec.ca/vaxiupdates/vaxi.json"
-wget --directory-prefix=$outputDirectory -O vaxi.json -- $url
+wget -O $outputDirectory/vaxi.json -- $url
