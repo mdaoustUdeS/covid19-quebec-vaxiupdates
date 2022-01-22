0ea594debb1683bbf95a3bebeb071384dc9de581
mdaoustUdeS <4569756-mdaoustUdeS@users.noreply.gitlab.com>
2022-01-22 16:07:03 -0500

    Remove slash after folder path

--- getUrls.sh
+++ getUrls.sh
@@ -2,13 +2,13 @@
 
 outputDirectory=update
 
-path="https://covid19.quebec.ca/vaxiupdates/"
+path="https://covid19.quebec.ca/vaxiupdates"
 file="vaxi.json"
 
 curl $path/$file -o $outputDirectory/$file
 jq . $outputDirectory/$file > $outputDirectory/${file%.*}-formated.${file##*.}
 
-path="https://covid19.quebec.ca/PreuveVaccinaleApi/issuer/.well-known/"
+path="https://covid19.quebec.ca/PreuveVaccinaleApi/issuer/.well-known"
 file="jwks.json"
 
 curl $path/$file -o $outputDirectory/$file
