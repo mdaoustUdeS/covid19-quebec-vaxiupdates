deb26915011822855f94e642175336b815d6fba1
mdaoustUdeS <54960692+mdaoustUdeS@users.noreply.github.com>
2022-01-22 14:53:04 -0500

    Add public key

--- getUrls.sh
+++ getUrls.sh
@@ -1,2 +1,5 @@
 url="https://covid19.quebec.ca/vaxiupdates/vaxi.json"
 wget -O update/vaxi.json -- $url
+
+url="https://covid19.quebec.ca/PreuveVaccinaleApi/issuer/.well-known/jwks.json"
+wget -O update/jwks.json -- $url
