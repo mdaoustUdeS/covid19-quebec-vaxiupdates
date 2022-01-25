9fc2cc1c6cfc056f311d198111ff24c002d7de15
mdaoustUdeS <54960692+mdaoustUdeS@users.noreply.github.com>
2022-01-25 01:54:03 -0500

    Add revocation list for second key

--- getUrls.sh
+++ getUrls.sh
@@ -4,12 +4,15 @@ outputDirectory=update
 
 path="https://covid19.quebec.ca/vaxiupdates"
 file="vaxi.json"
-
 curl $path/$file -o $outputDirectory/$file
 jq . $outputDirectory/$file > $outputDirectory/${file%.*}-formated.${file##*.}
 
 path="https://covid19.quebec.ca/PreuveVaccinaleApi/issuer/.well-known"
 file="jwks.json"
+curl $path/$file -o $outputDirectory/$file
+jq . $outputDirectory/$file > $outputDirectory/${file%.*}-formated.${file##*.}
 
+path="https://covid19.quebec.ca/preuvevaccinaleapi/issuer/.well-known/crl"
+file="sZ5ca2a73SgPl7aC9v4PyA4cR5zk9A6BhHX8I2CVNwM.json"
 curl $path/$file -o $outputDirectory/$file
 jq . $outputDirectory/$file > $outputDirectory/${file%.*}-formated.${file##*.}
